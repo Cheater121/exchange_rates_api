25abf2121ed0a0fcc23a8f3b4354e6ddfa1ffcf3
Cheaster121 <cheater1211@gmail.com>
2024-01-12 00:31:10 +0300

    Updated auto asyncio_mode

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -8,12 +10,15 @@ from main import app
 from app.db.models import Currency, Rate
 
 
-pytestmark = pytest.mark.asyncio
 TEST_DATABASE_URL = settings.DATABASE_URL
 
 
-@pytest.mark.asyncio
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session")
+def event_loop():
+    return asyncio.get_event_loop()
+
+
+@pytest.fixture(scope="module", autouse=True)
 async def setup_database():
     if settings.MODE == "TEST":
         engine = create_async_engine(TEST_DATABASE_URL, echo=True)
@@ -27,9 +32,8 @@ async def setup_database():
             await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.mark.asyncio
 async def test_update_rates():
-    async with AsyncClient(app=app, base_url="http://127.0.0.1") as ac:
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
         response = await ac.get("/api/update_rates")
 
     assert response.status_code == 200
@@ -37,19 +41,17 @@ async def test_update_rates():
     assert response.json().get("updated_at")
 
 
-@pytest.mark.asyncio
 async def test_get_last_update_datetime():
-    async with AsyncClient(app=app, base_url="http://127.0.0.1") as ac:
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
         response = await ac.get("/api/last_update")
 
     assert response.status_code == 200
     assert response.json().get("updated_at")
 
 
-@pytest.mark.asyncio
 async def test_convert_currencies():
-    async with AsyncClient(app=app, base_url="http://127.0.0.1") as ac:
-        response = await ac.post("http://127.0.0.1/api/convert",
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+        response = await ac.post("/api/convert",
                                  json={"from_currency_code": "USD", "to_currency_code": "EUR", "count": 10})
 
     assert response.status_code == 200
